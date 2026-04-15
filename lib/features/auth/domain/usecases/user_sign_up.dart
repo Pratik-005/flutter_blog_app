2820a86ff454fd55530013d337c287c33ca91cd8
Pratik Pisudde <pratik.pisudde.005@gmail.com>
2026-04-15 20:02:28 +0530

    implement domain layer for auth

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,29 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignupParams {
+  final String name;
+  final String email;
+  final String password;
+  UserSignupParams({
+    required this.email,
+    required this.name,
+    required this.password,
+  });
+}
+
+class UserSignUp implements Usecase<String, UserSignupParams> {
+  final AuthRepository authRepository;
+  UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignupParams params) async {
+    return await authRepository.signupWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
